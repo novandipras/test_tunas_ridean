74c84544c214a1b0c9691919bd2243e56924d90e
Pras <40951221+novandipras@users.noreply.github.com>
2025-09-17 10:49:57 +0700

    Update test.dart

--- test.dart
+++ test.dart
@@ -1,8 +1,8 @@
 import 'dart:math';
 
 void main() {
-  int n = Random().nextInt(10) + 1;
-  int m = Random().nextInt(10) + 1;
+  int n = Random().nextInt(5) + 1;
+  int m = Random().nextInt(5) + 1;
 
   print(n);
   print(m);
@@ -10,15 +10,15 @@ void main() {
   List<List<int>> x = [];
 
   for (int i = 0; i < n; i++) {
-    int a = Random().nextInt(1000) + 1;
-    int b = Random().nextInt(1000) + 1;
+    int a = Random().nextInt(5) + 1;
+    int b = Random().nextInt(5) + 1;
     x.add([a, b]);
   }
 
   x.sort((a, b) => a.first.compareTo(b.first));
 
   int o = x.fold(m, (prev, next) {
-    if (next[0] < prev) {
+    if (prev < next[0]) {
       return prev;
     } else {
       return prev + next[1];
